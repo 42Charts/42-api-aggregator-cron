d2803d4cab36b393545c4689a961f5be296004c2
Yoann FUKS <yfuks@student.42.fr>
2018-11-26 14:49:16 +0100

    Remove deprecated hosts from totallogtime

--- cronTasks/day/updateTotalLogtime.js
+++ cronTasks/day/updateTotalLogtime.js
@@ -2,7 +2,7 @@ var async = require('async');
 const mysql = require('../../libraries/database');
 
 const updateTotalLogsTimes = () => new Promise((resolve, reject) => {
-  let query = 'SELECT u.ID as id, SUM(l.logtimeInSeconds) as totalLogTimeInSeconds from USERS u INNER JOIN USERSCURSUS uc ON uc.userID=u.ID INNER JOIN LOCATIONS l ON l.userID=u.ID INNER JOIN CURSUS c ON c.ID=uc.cursusID WHERE c.ID=1 GROUP BY u.ID';
+  let query = 'SELECT u.ID as id, SUM(l.logtimeInSeconds) as totalLogTimeInSeconds from USERS u INNER JOIN USERSCURSUS uc ON uc.userID=u.ID INNER JOIN LOCATIONS l ON l.userID=u.ID INNER JOIN HOSTS h ON h.ID=l.hostID INNER JOIN CURSUS c ON c.ID=uc.cursusID WHERE h.deprecated=0 AND c.ID=1 GROUP BY u.ID';
   mysql.query(query, (err, result) => {
     if (err) {
       return reject(err);
